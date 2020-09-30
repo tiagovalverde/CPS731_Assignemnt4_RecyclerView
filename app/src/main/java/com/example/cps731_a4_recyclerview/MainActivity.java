d55f0d91ab336dea5ecfa70d422d0b073b32fc58
tiagovalverde <tvalverde@verto.ca>
2020-09-30 18:59:05 -0400

    first commit - android studio project setup

---
+++ app/src/main/java/com/example/cps731_a4_recyclerview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cps731_a4_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
