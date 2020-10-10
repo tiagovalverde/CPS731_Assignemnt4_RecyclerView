248598410c756b4882835ae5f002cdc8175e5086
tiagovalverde <tvalverde@verto.ca>
2020-10-09 23:57:53 -0400

    a5: add splash screen music with activities lifecycle methods

--- app/src/main/java/com/example/cps731_a4_recyclerview/SplashActivity.java
+++ app/src/main/java/com/example/cps731_a4_recyclerview/SplashActivity.java
@@ -3,8 +3,10 @@ package com.example.cps731_a4_recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,14 +15,40 @@ public class SplashActivity extends AppCompatActivity {
     Timer timerLoadingApp;
     private static final int TIMER_DURATION = 5000;
 
+    MediaPlayer mediaPlayerLoadingApp;
+    int playing;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        this.setMediaPlayer();
         this.setTimerTask();
+        this.startLoadingTimer();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mediaPlayerLoadingApp.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayerLoadingApp.stop();
+    }
+
+    private void startLoadingTimer() {
+        System.out.println(mediaPlayerLoadingApp);
         timerLoadingApp = new Timer();
         timerLoadingApp.schedule(timerTask, TIMER_DURATION);
+    }
+
+
+    private void setMediaPlayer() {
+        mediaPlayerLoadingApp = new MediaPlayer();
+        mediaPlayerLoadingApp = MediaPlayer.create(this, R.raw.mario_bros);
     }
 
     private void setTimerTask() {
